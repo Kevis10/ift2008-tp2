ab2c47eb6251eee61a304022447ae16148490844
Jean-Thomas Baillargeon <jean-thomas.baillargeon.1@ulaval.ca>
2017-06-24 23:31:09 -0400

    ajout des balise de doc pour ReseauInterurbain

--- ReseauInterurbain.cpp
+++ ReseauInterurbain.cpp
@@ -18,31 +18,66 @@
 namespace TP2
 {
 
+	/**
+	* \brief Constructeur du ReseauInterurbain
+	*
+	* \post Une instance du ReseauInterurbain est cree
+	* \param[in] nomReseau le nom du reseau
+	* \param[in] nbVilles le nombre de villes dans le reseau
+	*/
 	ReseauInterurbain::ReseauInterurbain(std::string nomReseau, size_t nbVilles)
 	:unReseau(nbVilles), nomReseau(nomReseau)
 	{
 
 	}
 
+	/**
+	* \brief Destructeur du ReseauInterurbain
+	*
+	* \post l'instance du ReseauInterurbain est detruite
+	*/
 	ReseauInterurbain::~ReseauInterurbain(){
 
 	}
 
+	/**
+	* \brief Modifie la grosseur du reseau
+	*
+	* \post Le reseau a une taille modifiee
+	* \param[in] nouvelleTailles la nouvelle taille du reseau
+	*/
 	void ReseauInterurbain::resize(size_t nouvelleTailles){
 		unReseau.resize(nouvelleTailles);
 	}
 
+
+	/**
+	* \brief Calcule le plus court chemin entre deux sommets selon une poderation en temps ou en $
+	*
+	* \post Le reseau a une taille modifiee
+	* \param[in] source le sommet de depart
+	* \param[in] destination le sommet a atteindre
+	* \param[in] dureeCout vrai si pondere par duree, faux si pondere par cout
+	*/
 	Chemin ReseauInterurbain::rechercheCheminDijkstra(const std::string & source, const std::string & destination, bool dureeCout) const {
 		Chemin compile_pls;
 		return compile_pls;
 	}
+
+
+	/**
+	* \brief Retourne les composantes fortement connexes d'un reseau
+	*/
 
 	std::vector<std::vector<std::string> > ReseauInterurbain::algorithmeKosaraju(){
 		std::vector<std::vector<std::string> > compile_pls;
 		return compile_pls;
 	}
 
-	// Méthode fournie
+	/**
+	* \brief Genere l'output en texte d'un reseau
+	*/
+
 	void ReseauInterurbain::chargerReseau(std::ifstream & fichierEntree)
 	{
 		std::string buff;
@@ -86,6 +121,5 @@ namespace TP2
 		}
 	}
 
-//À compléter par l'implémentation des autres méthodes demandées
 
-}//Fin du namespace
+}

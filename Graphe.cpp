c30d22de9b0f9439cc9f4fa20c54d6c63915e93e
Jean-Thomas Baillargeon <jean-thomas.baillargeon.1@ulaval.ca>
2017-06-24 20:56:59 -0400

    ajout des fonctions de base pour faire compiler le programme principal (aucun algo de fait mais les constructeurs devraient etre ok...

--- Graphe.cpp
+++ Graphe.cpp
@@ -11,10 +11,53 @@
 
 #include "Graphe.h"
 #include "AssertionException.h"
+#include "algorithm"
+
 //vous pouvez inclure d'autres librairies si c'est nécessaire
 
 namespace TP2
 {
+
+	Graphe::Graphe(size_t nbSommets)
+	:noms(), listesAdj(), nbSommets(nbSommets), nbArcs()
+	{
+
+
+	}
+
+	Graphe::~Graphe(){}
+
+
+	void Graphe::resize(size_t nouvelleTaille){
+		noms.resize(nouvelleTaille);
+		listesAdj.resize(nouvelleTaille);
+		ASSERTION(noms.size()==nouvelleTaille && listesAdj.size() == nouvelleTaille);
+	}
+
+	void Graphe::nommer(size_t sommet, const std::string & nom){
+		noms[sommet] = nom;
+	}
+
+	void Graphe::ajouterArc(size_t source, size_t destination,float duree, float cout){
+		//peut etre verifier que le sommet existe avant d'ajouter
+		//peut etre verifier que l'arc n'existe pas deja...
+
+		Ponderations ponderation_de_arc(cout,duree);
+		Arc arc_a_ajouter(destination,ponderation_de_arc);
+		arc_a_ajouter.poids = ponderation_de_arc;
+		listesAdj[source].push_back(arc_a_ajouter);
+		nbArcs ++;
+	}
+
+	size_t Graphe::getNumeroSommet(const std::string & nom) const{
+		for(size_t i =0 ; i<noms.size() ; i++){
+			if(noms[i]==nom){
+				return i;
+			}
+		}
+	}
+
+
 	//Mettez l'implémentation de vos méthodes ici.
 
 }//Fin du namespace
--- ReseauInterurbain.cpp
+++ ReseauInterurbain.cpp
@@ -10,6 +10,7 @@
  */
 #include <sstream>
 #include <fstream>
+#include <string>
 #include "ReseauInterurbain.h"
 #include "AssertionException.h"
 //vous pouvez inclure d'autres librairies si c'est nécessaire
@@ -17,49 +18,70 @@
 namespace TP2
 {
 
-// Méthode fournie
-void ReseauInterurbain::chargerReseau(std::ifstream & fichierEntree)
-{
-	std::string buff;
-
-	getline(fichierEntree, nomReseau);
-	nomReseau.erase(0, 20); // Enlève: Reseau Interurbain:
-
-	int nbVilles;
-
-	fichierEntree >> nbVilles;
-	getline(fichierEntree, buff); //villes
-
-	unReseau.resize(nbVilles);
-
-	getline(fichierEntree, buff); //Liste des villes
-
-	size_t i = 0;
-
-	while(getline(fichierEntree, buff) && buff != "Liste des trajets:")
+	ReseauInterurbain::ReseauInterurbain(std::string nomReseau, size_t nbVilles)
+	:unReseau(nbVilles), nomReseau(nomReseau)
 	{
-		unReseau.nommer(i, buff);
-		i++;
+
 	}
 
-	while(getline(fichierEntree, buff))
-	{
-		std::string source = buff;
-		getline(fichierEntree, buff);
-		std::string destination = buff;
+	ReseauInterurbain::~ReseauInterurbain(){
 
-		getline(fichierEntree, buff);
-		std::istringstream iss(buff);
-
-		float duree;
-		iss >> duree;
-
-		float cout;
-		iss >> cout;
-
-		unReseau.ajouterArc(unReseau.getNumeroSommet(source), unReseau.getNumeroSommet(destination), duree, cout);
 	}
-}
+
+
+	Chemin ReseauInterurbain::rechercheCheminDijkstra(const std::string & source, const std::string & destination, bool dureeCout) const {
+		Chemin compile_pls;
+		return compile_pls;
+	}
+
+	std::vector<std::vector<std::string> > ReseauInterurbain::algorithmeKosaraju(){
+		std::vector<std::vector<std::string> > compile_pls;
+		return compile_pls;
+	}
+
+	// Méthode fournie
+	void ReseauInterurbain::chargerReseau(std::ifstream & fichierEntree)
+	{
+		std::string buff;
+
+		getline(fichierEntree, nomReseau);
+		nomReseau.erase(0, 20); // Enlève: Reseau Interurbain:
+
+		int nbVilles;
+
+		fichierEntree >> nbVilles;
+		getline(fichierEntree, buff); //villes
+
+		unReseau.resize(nbVilles);
+
+		getline(fichierEntree, buff); //Liste des villes
+
+		size_t i = 0;
+
+		while(getline(fichierEntree, buff) && buff != "Liste des trajets:")
+		{
+			unReseau.nommer(i, buff);
+			i++;
+		}
+
+		while(getline(fichierEntree, buff))
+		{
+			std::string source = buff;
+			getline(fichierEntree, buff);
+			std::string destination = buff;
+
+			getline(fichierEntree, buff);
+			std::istringstream iss(buff);
+
+			float duree;
+			iss >> duree;
+
+			float cout;
+			iss >> cout;
+
+			unReseau.ajouterArc(unReseau.getNumeroSommet(source), unReseau.getNumeroSommet(destination), duree, cout);
+		}
+	}
 
 //À compléter par l'implémentation des autres méthodes demandées
 

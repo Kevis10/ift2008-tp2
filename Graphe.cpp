cc8d792c5fc43cf2d8572a47e64b9db2dcb5f7dd
Jean-Thomas Baillargeon <jean-thomas.baillargeon.1@ulaval.ca>
2017-06-24 23:24:35 -0400

    implementation de toutes les methodes requises pour le graph

--- Graphe.cpp
+++ Graphe.cpp
@@ -17,17 +17,32 @@
 
 namespace TP2
 {
-
+	 /**
+	 * \brief Constructeur du graph
+	 *
+	 * \post Une instance du graphe est cree
+	 * \param[in] nbSommets le nombre de sommets du graphe
+	 */
 	Graphe::Graphe(size_t nbSommets)
-	:noms(), listesAdj(), nbSommets(nbSommets), nbArcs()
+	:noms(nbSommets), listesAdj(nbSommets), nbSommets(nbSommets), nbArcs()
 	{
-
 
 	}
 
+	 /**
+	 * \brief destructeur du graph
+	 *
+	 * \post Le graph est detruit
+	 */
 	Graphe::~Graphe(){}
 
 
+	 /**
+	 * \brief Change le nombre de sommets du graphe
+	 *
+	 * \post Le graphe a maintenant plus de sommets
+	 * \param[in] nouvelleTaille le nouveau nombre de sommets du graphe
+	 */
 	void Graphe::resize(size_t nouvelleTaille){
 		noms.resize(nouvelleTaille);
 		listesAdj.resize(nouvelleTaille);
@@ -35,21 +50,120 @@ namespace TP2
 		ASSERTION(noms.size()==nouvelleTaille && listesAdj.size() == nouvelleTaille);
 	}
 
+	/**
+	 * \brief Affecte un nom a un sommet
+	 *
+	 * \post Le sommet est nomme
+	 * \param[in] sommet le sommet a modifier
+	 * \param[in] le nom a affecter au sommet
+	 */
 	void Graphe::nommer(size_t sommet, const std::string & nom){
 		noms[sommet] = nom;
 	}
 
+	/**
+	 * \brief Ajoute un arc au graphe
+	 *
+	 * \post L'arc est ajoute
+	 * \param[in] source le point de depart
+	 * \param[in] destination le point d'arrivee
+	 * \param[in] duree La duree de l'arc en temps
+	 * \param[in] cout Le cout de traverser l'arc en $
+	 */
 	void Graphe::ajouterArc(size_t source, size_t destination,float duree, float cout){
-		//peut etre verifier que le sommet existe avant d'ajouter
 		//peut etre verifier que l'arc n'existe pas deja...
 
-		Ponderations ponderation_de_arc(cout,duree);
+		Ponderations ponderation_de_arc(duree,cout);
 		Arc arc_a_ajouter(destination,ponderation_de_arc);
 		arc_a_ajouter.poids = ponderation_de_arc;
 		listesAdj[source].push_back(arc_a_ajouter);
 		nbArcs ++;
 	}
 
+	/**
+	 * \brief Enleve un arc au graphe
+	 *
+	 * \post L'arc est enleve
+	 * \param[in] source le point de depart
+	 * \param[in] destination le point d'arrivee
+	 */
+	void Graphe::enleverArc(size_t source, size_t destination){
+
+		std::list<Arc>::iterator arc_a_enlever;
+		for(std::list<Arc>::iterator i = listesAdj[source].begin(); i != listesAdj[source].end(); i++ ){
+			if(i->destination==destination){
+				arc_a_enlever = i;
+			}
+		}
+		listesAdj[source].erase(arc_a_enlever);
+	}
+
+	/**
+	 * \brief Retourne si l'arc existe ou non
+	 *
+	 * \param[in] source le point de depart
+	 * \param[in] destination le point d'arrivee
+	 */
+	bool Graphe::arcExiste(size_t source, size_t destination) const{
+		ASSERTION(source<nbSommets);
+
+		for(auto arc: listesAdj[source]){
+			if(arc.destination==destination){
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	/**
+	 * \brief Retourne la liste des sommets adjacents d'un sommet
+	 *
+	 * \param[in] source le point de depart
+	 */
+	std::vector<size_t> Graphe::listerSommetsAdjacents(size_t sommet) const{
+		ASSERTION(sommet<nbSommets);
+		std::vector<size_t> liste_des_adjacents;
+		for(auto arc: listesAdj[sommet]){
+			liste_des_adjacents.push_back(arc.destination);
+		}
+		return liste_des_adjacents;
+	}
+
+	/**
+	 * \brief Retourne le nom d'un sommet
+	 *
+	 * \param[in] size_t l'index du sommet
+	 */
+	std::string Graphe::getNomSommet(size_t sommet) const{
+		ASSERTION(sommet<nbSommets);
+		return noms[sommet];
+	}
+
+
+	/**
+	 * \brief Retourne le nombre de sommet du graphe
+	 *
+	*/
+	int Graphe::getNombreSommets() const{
+		return nbSommets;
+	}
+
+	/**
+	* \brief Retourne le nombre des arcs du graphe
+	*
+	*/
+	//
+	int Graphe::getNombreArcs() const{
+		return nbArcs;
+	}
+
+
+	/**
+	 * \brief Retourne l'index du sommet
+	 *
+	 * \param[in] size_t le nom du sommet
+	 */
 	size_t Graphe::getNumeroSommet(const std::string & nom) const{
 		for(size_t i =0 ; i<noms.size() ; i++){
 			if(noms[i]==nom){
@@ -58,7 +172,21 @@ namespace TP2
 		}
 	}
 
+	/**
+	 * \brief Retourne la ponderation des couts d'un arc donnee
+	 *
+	 * \param[in] source le point de depart
+	 * \param[in] destination le point d'arrivee
+	 */
+	Ponderations Graphe::getPonderationsArc(size_t source, size_t destination) const{
+		if(arcExiste(source,destination)){
+			for(auto arc: listesAdj[source]){
+				if(arc.destination==destination){
+					return arc.poids;
+				}
+			}
+		}
+	}
 
-	//Mettez l'implémentation de vos méthodes ici.
 
 }//Fin du namespace
